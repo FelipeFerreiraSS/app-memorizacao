c6ba402145223b758185bd2cf7e993b9dbc1bfda
Felipe Ferreira <filipiferreirasilva789@gmail.com>
2023-08-11 17:40:21 -0300

    Update Login.js

--- components/Login.js
+++ components/Login.js
@@ -1,5 +1,9 @@
 import React, { useState } from 'react'
 import { useAuth } from '../context/AuthContext'
+import Link from "next/link";
+
+import Image from 'next/image'
+import logoSvg from "../public/logo.svg";
 
 export default function Login() {
     const [email, setEmail] = useState('')
@@ -31,15 +35,17 @@ export default function Login() {
     return (
         <div className="mx-auto max-w-screen-xl px-4 py-16 sm:px-6 lg:px-8">
           <div className="mx-auto max-w-lg">
-            <h1 className="text-center text-2xl font-bold text-indigo-600 sm:text-3xl">
-              Get started today
-            </h1>
-
-            <p className="mx-auto mt-4 max-w-md text-center text-gray-500">
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Obcaecati sunt
-              dolores deleniti inventore quaerat mollitia?
-            </p>
-
+            <div className="flex justify-center max-w-lg">
+              <Link href="/">
+                <Image
+                className=''
+                src={logoSvg}
+                width={180}
+                height={180}
+                alt="Picture of the author"
+                />
+              </Link>
+            </div>
             <div
               className="mb-0 mt-6 space-y-4 rounded-lg p-4 shadow-2xl sm:p-6 lg:p-8"
             >
@@ -144,7 +150,7 @@ export default function Login() {
               </button>
 
               <p className="text-center text-sm text-gray-500">
-                {!isLoggingIn ? 'Já tem uma conta ' : 'Não tem uma conta? '}
+                {!isLoggingIn ? 'Já tem uma conta? ' : 'Não tem uma conta? '}
                 <span className="underline text-blue-500 cursor-pointer" onClick={() => setIsLoggingIn(!isLoggingIn)}>{!isLoggingIn ? 'Entre com sua conta' : 'Criar conta'}</span>
               </p>
             </div>
