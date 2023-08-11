415b0842ec63e7cdf7edca732589837077732c1e
Felipe Ferreira <filipiferreirasilva789@gmail.com>
2023-08-11 17:23:38 -0300

    Create 404.js

---
+++ pages/404.js
@@ -0,0 +1,24 @@
+import react from 'react'
+import Link from "next/link"
+
+export default function Error() {
+  return (
+  	<div className="grid h-screen px-4 bg-white place-content-center">
+	  <div className="text-center">
+	    <h1 className="font-black text-gray-400 text-9xl">404</h1>
+
+	    <p className="text-2xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+	      Uh-oh!
+	    </p>
+
+	    <p className="mt-4 text-gray-500">Não conseguimos encontrar essa página.</p>
+
+	    <Link href="/"
+	      className="inline-block px-5 py-3 mt-6 text-sm font-medium text-white bg-indigo-600 rounded hover:bg-indigo-700 focus:outline-none focus:ring"
+	    >
+	      Voltar para a Home
+	    </Link>
+	  </div>
+	</div>
+  ) 
+}
