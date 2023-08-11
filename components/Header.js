455d4f7e62dd410fe5297d8e356872fa97f90307
Felipe Ferreira <filipiferreirasilva789@gmail.com>
2023-08-11 17:30:14 -0300

    Menu mobile

--- components/Header.js
+++ components/Header.js
@@ -90,7 +90,7 @@ export default function Header() {
                     <div className="block md:hidden">
                     <button
                         onClick={toggleDropdown}
-                        className="rounded bg-gray-100 p-2 text-gray-600 transition hover:text-gray-600/75"
+                        className={`rounded bg-gray-100 p-2 text-gray-600 transition hover:text-gray-600/75 ${isOpen ? 'hidden' : 'block'}`}
                     >
                         <svg
                         xmlns="http://www.w3.org/2000/svg"
@@ -108,26 +108,31 @@ export default function Header() {
                         </svg>
                     </button>
                     {isOpen && (
-                        <div className={`w-full mt-32 bg-white ${isOpen ? 'block' : 'hidden'}`}>
-                            <ul className="grid gap-5 justify-items-end text-sm">
+                        <button
+                            onClick={toggleDropdown}
+                            className="rounded w-10 rounded-md bg-gray-50 p-2 text-gray-600 transition hover:text-gray-600/75"
+                        >
+                            <Image
+                            className='h-5 w-5'
+                            src={close}
+                            width={20}
+                            height={20}
+                            alt="Picture of the author"
+                            />
+                        </button>
+                    )}
+                    </div>
+                </div>
+                </div>
+                {isOpen && (
+                    <div className={`absolute sm:hidden w-11/12 mr-2 bg-gray-100 border-2 pb-5 rounded-md border-gray-200 ${isOpen ? 'block' : 'hidden'}`}>
+                            <ul className="grid gap-5 mr-2 justify-items-end text-sm">
                                 <li>
-                                    <button
-                                        onClick={toggleDropdown}
-                                        className="rounded w-10 rounded-md bg-gray-100 p-2 text-gray-600 transition hover:text-gray-600/75"
-                                    >
-                                        <Image
-                                        className='h-5 w-5'
-                                        src={close}
-                                        width={20}
-                                        height={20}
-                                        alt="Picture of the author"
-                                        />
-                                    </button>
                                 </li>
                                 <li>
                                 <a
                                     onClick={toggleDropdown}
-                                    className="rounded-lg bg-gray-100 px-4 py-2 text-sm font-medium text-gray-700"
+                                    className="rounded-lg bg-white px-4 py-2 text-sm font-medium text-gray-700"
                                     href="/#about"
                                 >
                                     Sobre
@@ -137,7 +142,7 @@ export default function Header() {
                                 <li>
                                 <a
                                     onClick={toggleDropdown}
-                                    className="mb-5 rounded-lg bg-gray-100 px-4 py-2 text-sm font-medium text-gray-700"
+                                    className="mb-7 rounded-lg bg-white px-4 py-2 text-sm font-medium text-gray-700"
                                     href="/#functions"
                                 >
                                     Funçôes
@@ -147,7 +152,7 @@ export default function Header() {
                                 <li>
                                 <a
                                     onClick={toggleDropdown}
-                                    className="mb-5 rounded-lg bg-gray-100 px-4 py-2 text-sm font-medium text-gray-700"
+                                    className="mb-7 rounded-lg bg-white px-4 py-2 text-sm font-medium text-gray-700"
                                     href="/#price"
                                 >
                                     Preço
@@ -157,7 +162,7 @@ export default function Header() {
                                 <li>
                                 <a
                                     onClick={toggleDropdown}
-                                    className="mb-5 rounded-lg bg-gray-100 px-4 py-2 text-sm font-medium text-gray-700"
+                                    className="mb-7 rounded-lg bg-white px-4 py-2 text-sm font-medium text-gray-700"
                                     href="/#faq"
                                 >
                                     FAQ
@@ -166,10 +171,7 @@ export default function Header() {
                             </ul>
 
                         </div>
-                    )}
-                    </div>
-                </div>
-                </div>
+                )}
             </div>
         </header>
     )
