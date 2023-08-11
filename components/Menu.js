7e91e591a835fb6e896ed9dd136e707d06f4ca82
Felipe Ferreira <filipiferreirasilva789@gmail.com>
2023-08-11 17:33:59 -0300

    Update Menu.js

--- components/Menu.js
+++ components/Menu.js
@@ -1,7 +1,9 @@
 import React, { useState, useEffect } from 'react'
 import { useAuth } from '../context/AuthContext'
 import { getAuth } from "firebase/auth";
+import Link from "next/link";
 
+import logoSvg from "../public/logo.svg";
 import Image from 'next/image';
 import account_circle from "../public/account_circle.svg";
 import logout_icon from "../public/logout_icon.svg";
@@ -25,19 +27,20 @@ export default function Menu() {
 
   return (
     <header className="bg-gray-50">
-      <div className="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 lg:px-8">
-        <div className="flex items-center justify-end gap-4">
-          <div className="flex items-center gap-4">
-            <a href="#" className="block shrink-0 rounded-full bg-white p-2.5 text-gray-600 shadow-sm hover:text-gray-700">
-                <span className="sr-only">Profile</span>
-                <Image
-                src={account_circle}
-                width={20}
-                height={20}
-                alt="Picture of the author"
-                />
-            </a>
+      <div className="mx-auto max-w-screen-xl px-4 pt-3 pb-8 sm:px-6 lg:px-8">
+        <div className="flex items-center gap-4">
+          <Link href="/">
+            <Image
+            src={logoSvg}
+            width={200}
+            height={200}
+            alt="Picture of the author"
+            />
+          </Link>
+        </div>
 
+        <div>
+          <div className="flex items-center justify-end gap-4">
             <span
                 aria-hidden="true"
                 className="block h-6 w-px rounded-full bg-gray-200"
@@ -56,22 +59,14 @@ export default function Menu() {
               <span className="sr-only">logout</span>
               <Image
                 src={logout_icon}
+                className="hover:scale-125 duration-300"
                 width={20}
                 height={20}
                 alt="Picture of the author"
                 />
             </button>
           </div>
-        </div>
-
-        <div class="mt-2">
-          <h1 class="text-2xl font-bold text-gray-900 sm:text-3xl">
-            App memorização
-          </h1>
-
-          <p class="mt-1.5 text-sm text-gray-500">
-            Adicione cartões com palavras e frases para se lembrar depois
-          </p>
+          
         </div>
       </div>
     </header>
