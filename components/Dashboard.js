527f991f71fef55789c8eca3ecfdb3bfbcde9dea
Felipe Ferreira <filipiferreirasilva789@gmail.com>
2023-08-22 20:17:36 -0300

    updating card display

--- components/Dashboard.js
+++ components/Dashboard.js
@@ -42,36 +42,45 @@ export default function Dashboard() {
         	setTranslation={setTranslation}
         	handleButtonClick={handleButtonClick}
         />
-        {(loading) && (
+        {loading ? (
           <div className='flex-1 grid place-items-center'>
             <Image
-            className='animate-spin'
-            src={refresh}
-            width={30}
-            height={30}
-            alt="Picture of the author"
+              className='animate-spin'
+              src={refresh}
+              width={30}
+              height={30}
+              alt="Picture of the author"
             />
           </div>
-        )}
-        {(!loading) && (
+        ) : (
           <>
-            <div className='m-auto mt-12 grid grid-cols-1 gap-4 md:grid-cols-3 md:mt-0 lg:grid-cols-4 lg:gap-8'>
-              {Object.keys(allCards).map((card, i) => {
-                return (
-                  <Cards 
-                    key={i.toString()}
-                  	allCards={allCards}
-                  	card={card}
-                  	edit={edit}
-                  	edittedValue={edittedValue}
-                  	setEdittedValue={setEdittedValue}
-                  	handleEditCard={handleEditCard}
-                  	handleAddEdit={handleAddEdit}
-                  	handleDelete={handleDelete}
-                  />
-                )
-              })}
-            </div>
+            {Object.keys(allCards).length === 0 ? (
+              <div className="w-2/5 m-auto rounded-xl border border-gray-300 bg-white p-4 shadow-xl">
+                <strong className="block font-medium text-gray-900"> Nenhum cartão adicionado ainda </strong>
+                <p className="mt-1 text-sm text-gray-700">
+                  Utilize os comandos acima para adicionar um novo cartão.
+                </p>
+              </div>
+            ) : (
+              <div className='m-auto mt-12 grid grid-cols-1 gap-4 md:grid-cols-3 md:mt-0 lg:grid-cols-4 lg:gap-8'>
+                {Object.keys(allCards).map((card, i) => {
+                  console.log(card)
+                  return (
+                    <Cards 
+                      key={i.toString()}
+                      allCards={allCards}
+                      card={card}
+                      edit={edit}
+                      edittedValue={edittedValue}
+                      setEdittedValue={setEdittedValue}
+                      handleEditCard={handleEditCard}
+                      handleAddEdit={handleAddEdit}
+                      handleDelete={handleDelete}
+                    />
+                  )
+                })}
+              </div>
+            )}
           </>
         )}
       </div>
