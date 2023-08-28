4b4a70199087bf58e07a959f60da7e7d8d76a09c
Felipe Ferreira <filipiferreirasilva789@gmail.com>
2023-08-28 19:38:10 -0300

    view user profile

--- components/Menu.js
+++ components/Menu.js
@@ -10,12 +10,18 @@ import logout_icon from "../public/logout_icon.svg";
 import monitoring from "../public/monitoring.svg";
 
 import ActivityChart from './ActivityChart.js'
+import EditProfile from './EditProfile.js'
+import LevelUser from './LevelUser.js'
 
 export default function Menu({ allActivities }) {
   const { logout } = useAuth()
 
   const [userEmail, setUserEmail] = useState(null);
+  const [userName, setUserName] = useState(null);
+  const [userPhoto, setUserPhoto] = useState(null);
+  const [userDefaultName, setUserDefaultName] = useState(null);
   const [modalOpen, setModalOpen] = useState(false)
+  const [modalOpenProfile, setModalOpenProfile] = useState(false)
 
   const openModal = () => {
     setModalOpen(true)
@@ -25,6 +31,14 @@ export default function Menu({ allActivities }) {
     setModalOpen(false)
   }
 
+  const openModalProfile = () => {
+    setModalOpenProfile(true)
+  }
+
+  const closeModalProfile = () => {
+    setModalOpenProfile(false)
+  }
+
   useEffect(() => {
     const auth = getAuth();
     const user = auth.currentUser;
@@ -32,6 +46,14 @@ export default function Menu({ allActivities }) {
     if (user !== null) {
       user.providerData.forEach((profile) => {
         setUserEmail(profile.email);
+        setUserName(profile.displayName)
+        setUserPhoto(profile.photoURL)
+
+        const parts = profile.email.split('@');
+        if (parts.length === 2) {
+          const username = parts[0];
+          setUserDefaultName(username);
+        }
       });
     }
   }, []);
@@ -50,9 +72,59 @@ export default function Menu({ allActivities }) {
             />
           </Link>
         </div>
-
         <div>
           <div className="flex items-center justify-end gap-4">
+            <LevelUser 
+              allActivities={allActivities}
+            />
+            <div className="flex">
+              <div className="w-16 h-16 mr-3 m-auto rounded-full overflow-hidden relative">
+                {(userPhoto === null) ? (
+                  <img 
+                    src="https://images.unsplash.com/photo-1566301587328-95b65dc1df77?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80" 
+                    className="rounded-full object-cover"
+                    alt="Imagem do usuário" 
+                  />
+                ) : (
+                  <div className="rounded-full overflow-hidden" style={{ width: '100%', height: '100%' }}>
+                    <img 
+                      src={userPhoto}
+                      className="w-full h-full object-cover"
+                      alt="Imagem do usuário" 
+                    />
+                  </div>
+                )} 
+
+              </div>
+              <div>
+                {(userName === null) ? (
+                  <p className="text-lg mb-1 text-black font-semibold">
+                    {userDefaultName}
+                  </p>
+                ) : (
+                  <p className="text-xl mb-1 text-black font-semibold">
+                    {userName}
+                  </p>
+                )}
+                <p className="text-sm mb-1 text-gray-500">
+                  {userEmail}
+                </p>
+                <button
+                  onClick={openModalProfile}
+                  className="rounded-lg bg-white p-1 text-black shadow-sm hover:text-gray-700 text-sm"
+                >
+                  Editar
+                </button>
+                <EditProfile 
+                  isOpen={modalOpenProfile} 
+                  onClose={closeModalProfile}
+                />
+              </div>
+            </div>
+            <span
+              aria-hidden="true"
+              className="block h-12 w-px rounded-full bg-gray-400"
+            ></span>
             <button
               className="block shrink-0 rounded-full bg-white p-2.5 text-gray-600 shadow-sm hover:text-gray-700"
               onClick={openModal}
@@ -65,19 +137,12 @@ export default function Menu({ allActivities }) {
               alt="Picture of the author"
               />
             </button>
+
             <ActivityChart 
               isOpen={modalOpen} 
               onClose={closeModal} 
               allActivities={allActivities} 
             />
-            <span
-                aria-hidden="true"
-                className="block h-6 w-px rounded-full bg-gray-200"
-            ></span>
-
-            <p className="mt-1.5 text-sm text-gray-500">
-              {userEmail}
-            </p>
 
             <button
               className="block shrink-0 rounded-full bg-white p-2.5 text-gray-600 shadow-sm hover:text-gray-700"
